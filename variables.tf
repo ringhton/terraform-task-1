b82afc5a6285c82840d55057d3a37553ac65bec2
Andrey Tarasov <ringhton555@gmail.com>
2023-11-09 22:00:34 +0300

    Add variables.tf
    Delete comments and add to repository

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "token" {
+  type = string
+}
+
+variable "base_url" {
+  type = string
+  default = "https://gitlab.rebrainme.com/api/v4/"
+}
+
+variable "my_group" {
+  type = string
+}
+
+variable "my_subgroup" {
+  type = string
+}
+
+variable "pubkey" {
+  type = string
+}
+
